e5bbe80e048812adc7843780491cee2e0986d36d
Nijaz Andelić <77886732+AndNijaz@users.noreply.github.com>
2025-04-09 02:03:42 +0200

    finished branch except resposnive - will do tmrw

--- src/app/features/auth/user-info/user-info.component.ts
+++ src/app/features/auth/user-info/user-info.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputComponent } from '../../../shared/components/input/input.component';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-user-info',
   standalone: true,
-  imports: [FormsModule, InputComponent],
+  imports: [FormsModule, InputComponent, ButtonComponent],
   templateUrl: './user-info.component.html',
 })
 export class UserInfoComponent {
@@ -13,5 +14,13 @@ export class UserInfoComponent {
   formSurname: string = '';
   formPhoneNumber: string = '';
   formAddress: string = '';
-  onSubmit() {}
+  acceptPolicy: boolean = false;
+
+  onSubmit() {
+    console.log(this.formName);
+    console.log(this.formSurname);
+    console.log(this.formPhoneNumber);
+    console.log(this.formAddress);
+    console.log(this.acceptPolicy);
+  }
 }
