ebceee724da1a290028d3d9f3eab396ceaaf393c
Nikola Obradovic <nikola.obradovic@stu.ssst.edu.ba>
2025-04-10 03:55:01 +0200

    service-reservation-page completed

--- src/app/features/cleaner/cleaner-page/cleaner-page.component.ts
+++ src/app/features/cleaner/cleaner-page/cleaner-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl  } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -7,7 +7,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import {DateSelectorComponent} from '../../../shared/components/date-selector/date-selector.component';
 import {TimeSelectorComponent} from '../../../shared/components/time-selector/time-selector.component';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-cleaner-page',
   templateUrl: './cleaner-page.component.html',
@@ -25,7 +25,7 @@ import {TimeSelectorComponent} from '../../../shared/components/time-selector/ti
 })
 export class CleanerPageComponent implements OnInit {
   mainForm!: FormGroup;
-
+  submitted = false;
   get dateControl(): FormControl {
     return this.mainForm.get('date') as FormControl;
   }
@@ -39,7 +39,8 @@ export class CleanerPageComponent implements OnInit {
   }
 
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder,   private router: Router,
+  ) {}
 
   ngOnInit(): void {
     // Build the form controls:
@@ -52,11 +53,27 @@ export class CleanerPageComponent implements OnInit {
   }
 
   onSubmit(): void {
+    this.submitted = true;
     if (this.mainForm.valid) {
+//change 1 to :id when everything is ready
+      this.router.navigate([`cleaner/1/reserve`]);
       console.log('Form Values:', this.mainForm.value);
       // Do whatever (e.g., send to API)
     } else {
       console.warn('Form is invalid');
+      this.markAllControlsAsTouched();
+
     }
   }
-}
+
+  private markAllControlsAsTouched(): void {
+    Object.values(this.mainForm.controls).forEach(control => {
+      if (control instanceof FormGroup) {
+        this.markAllControlsAsTouched();
+      } else {
+        control.markAsTouched();
+      }
+    });
+
+
+}}
