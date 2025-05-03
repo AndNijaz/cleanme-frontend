7f808d29696a97de9706004b017745b8b9279b09
bejtoni <bejtic.alen@gmail.com>
2025-05-03 20:59:14 +0200

    ADDED: Navbar component variants

--- src/app/shared/navbar/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar/navbar.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ContainerComponent } from '../../components/container/container.component';
 import { ButtonComponent } from '../../components/button/button.component';
 import { RouterModule } from '@angular/router';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [ContainerComponent, ButtonComponent, RouterModule],
+  imports: [ContainerComponent, ButtonComponent, RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   standalone: true,
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  @Input() variant: 'default' | 'dashboard' = 'default';
+}
