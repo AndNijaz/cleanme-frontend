97e68294e849cfe8cae57dccc47f5ee9a32162e9
AndNijaz <andelicnijaz@gmail.com>
2025-06-01 09:44:30 +0200

    deploy

--- src/environemnts/environment.ts
+++ src/environemnts/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   api: {
-    reservation: 'https://cleanme-8cbf0906d144.herokuapp.com/:8080/reservation',
-    review: 'https://cleanme-8cbf0906d144.herokuapp.com/:8080/review',
-    baseURL: 'https://cleanme-8cbf0906d144.herokuapp.com/:8080',
+    reservation: 'https://cleanme-8cbf0906d144.herokuapp.com/reservation',
+    review: 'https://cleanme-8cbf0906d144.herokuapp.com/review',
+    baseURL: 'https://cleanme-8cbf0906d144.herokuapp.com',
   },
 };
